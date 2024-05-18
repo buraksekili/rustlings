2b691b46859ae8c1df13e862c595bc11a0f5ab76
Burak Sekili <buraksekili@gmail.com>
2024-05-18 11:48:54 +0300

    move semantics - wip
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -7,11 +7,13 @@
 //
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = vec![10, 20, 30 ,40]
+    let v: Vec<i32> = vec![10, 20, 30, 40];
+    // let mut v: Vec<i32> = Vec::new();
+    // for num in &a {
+    //     v.push(*num);
+    // }
 
     (a, v)
 }
